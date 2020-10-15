f2c4299dd1dd076abffe96ffaf9c5266ef5fc4ea
Jack Clarke <jackclarke95@gmail.com>
2020-10-15 15:42:33 +0100

    revert data changes

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1,404 +1,409 @@
 const Data = {
-  "characters": [
+  characters: [
     {
-      "id": 34044602,
+      id: 34044602,
       "player-dndbeyond-name": "Proxysaurus",
       "avatar-link": "12923/282/1581111423-34044602.jpeg",
-      "name": "Arthur Scone-Fopley",
-      "nickname": "Arthur",
-      "race": "Human",
-      "classes": [
-        { "class": "Fighter", "level": 5 },
-        { "class": "Barbarian", "level": 1 }
+      name: "Arthur Scone-Fopley",
+      nickname: "Arthur",
+      race: "Human",
+      classes: [
+        { class: "Fighter", level: 5 },
+        { class: "Barbarian", level: 1 },
       ],
       "starting-level": 6,
       "session-count": 3,
-      "next-session": "01/10/20"
+      "next-session": "01/10/20",
     },
     {
-      "id": 26128406,
+      id: 26128406,
       "player-dndbeyond-name": "SunRester",
       "avatar-link": "9608/256/637216255844292177.jpeg",
-      "name": "Babel",
-      "race": "Kenku",
-      "classes": [{ "class": "Artificer", "level": 7 }],
+      name: "Babel",
+      race: "Kenku",
+      classes: [{ class: "Artificer", level: 7 }],
       "session-count": 12,
-      "starting-level": 3
+      "starting-level": 3,
     },
     {
-      "id": 30038677,
+      id: 30038677,
       "player-dndbeyond-name": "AnAxeCalledYorik",
       "avatar-link": "9107/446/637196227255761865.jpeg",
-      "name": "Toussaint Baptiste Lafayette",
-      "nickname": "Baptiste",
-      "race": "Elf",
-      "subrace": "High",
-      "classes": [{ "class": "Rogue", "level": 5 }],
+      name: "Toussaint Baptiste Lafayette",
+      nickname: "Baptiste",
+      race: "Elf",
+      subrace: "High",
+      classes: [{ class: "Rogue", level: 5 }],
       "starting-level": 3,
-      "session-count": 5
+      "session-count": 5,
     },
     {
-      "id": 26119200,
+      id: 26119200,
       "player-dndbeyond-name": "FreemDeem",
       "avatar-link": "17/428/636377886920052559.jpeg",
-      "name": "Big Percy",
-      "race": "Half-orc",
-      "classes": [{ "class": "Fighter", "level": 8 }],
+      name: "Big Percy",
+      race: "Half-orc",
+      classes: [{ class: "Fighter", level: 8 }],
       "session-count": 14,
-      "starting-level": 3
+      "starting-level": 3,
     },
     {
-      "id": 27850604,
+      id: 27850604,
       "player-dndbeyond-name": "Chaia01",
       "avatar-link": "10265/600/637240973238597441.png",
-      "name": "Chaia Millicent Winterstorm",
-      "nickname": "Chaia",
-      "race": "Elf",
-      "subrace": "Wood",
-      "classes": [{ "class": "Druid", "level": 6 }],
+      name: "Chaia Millicent Winterstorm",
+      nickname: "Chaia",
+      race: "Elf",
+      subrace: "Wood",
+      classes: [{ class: "Druid", level: 6 }],
       "session-count": 9,
-      "starting-level": 3
+      "starting-level": 3,
     },
     {
-      "id": 34185467,
+      id: 34185467,
       "player-dndbeyond-name": "JackClarke",
       "avatar-link": "12289/752/1581111423-34185467.jpeg",
-      "name": "Eslyn Juhlenath",
-      "nickname": "Eslyn",
-      "race": "Triton",
-      "classes": [{ "class": "Sorcerer", "level": 6 }],
+      name: "Eslyn Juhlenath",
+      nickname: "Eslyn",
+      race: "Triton",
+      classes: [{ class: "Sorcerer", level: 6 }],
       "session-count": 1,
-      "starting-level": 6
+      "starting-level": 6,
     },
     {
-      "id": 34189033,
+      id: 34189033,
       "player-dndbeyond-name": "Proxysaurus",
       "avatar-link": "13124/972/1581111423-34189033.jpeg",
-      "name": "Inquisitor Lynchmire",
-      "nickname": "Lynchmire",
-      "race": "Half-elf",
-      "subrace": "Drow",
-      "classes": [{ "class": "Warlock", "level": 6 }],
+      name: "Inquisitor Lynchmire",
+      nickname: "Lynchmire",
+      race: "Half-elf",
+      subrace: "Drow",
+      classes: [{ class: "Warlock", level: 6 }],
       "session-count": 1,
-      "starting-level": 6
+      "starting-level": 6,
     },
     {
-      "id": 33132303,
+      id: 33132303,
       "player-dndbeyond-name": "BlackRoseSarah89",
       "avatar-link": "11957/368/1581111423-33132303.jpeg",
-      "name": "Ember",
-      "race": "Genasi",
-      "subrace": "Fire",
-      "classes": [{ "class": "Cleric", "level": 5 }],
+      name: "Ember",
+      race: "Genasi",
+      subrace: "Fire",
+      classes: [{ class: "Cleric", level: 5 }],
       "session-count": 3,
-      "starting-level": 5
+      "starting-level": 5,
     },
     {
-      "id": 35382595,
+      id: 35382595,
       "player-dndbeyond-name": "SchemingPnda",
       "avatar-link": "12649/693/1581111423-35382595.jpeg",
-      "name": "Grimshroom",
-      "race": "Goblin",
-      "classes": [{ "class": "Druid", "level": 6 }],
+      name: "Grimshroom",
+      race: "Goblin",
+      classes: [{ class: "Druid", level: 6 }],
       "session-count": 1,
-      "starting-level": 6
+      "starting-level": 6,
     },
     {
-      "id": 26375809,
+      id: 26375809,
       "player-dndbeyond-name": "Brewhan",
       "avatar-link": "18/19/636378975313015155.jpeg",
-      "name": "Raahm Bru'Han",
-      "nickname": "Raahm",
-      "race": "Human",
-      "classes": [{ "class": "Barbarian", "level": 3 }],
+      name: "Raahm Bru'Han",
+      nickname: "Raahm",
+      race: "Human",
+      classes: [{ class: "Barbarian", level: 3 }],
       "session-count": 1,
-      "starting-level": 3
+      "starting-level": 3,
     },
     {
-      "id": 35316039,
+      id: 35316039,
       "player-dndbeyond-name": "Benjywa",
       "avatar-link": "12923/301/1581111423-35316039.jpeg",
-      "name": "Hans Eleganza",
-      "nickname": "Hans",
-      "race": "Halfling",
-      "subrace": "Lightfoot",
-      "classes": [
-        { "class": "Bard", "level": 5 },
-        { "class": "Warlock", "level": 1 }
+      name: "Hans Eleganza",
+      nickname: "Hans",
+      race: "Halfling",
+      subrace: "Lightfoot",
+      classes: [
+        { class: "Bard", level: 5 },
+        { class: "Warlock", level: 1 },
       ],
       "session-count": 1,
-      "starting-level": 6
+      "starting-level": 6,
     },
     {
-      "id": 26125998,
+      id: 26125998,
       "player-dndbeyond-name": "TheKingsmantm",
       "avatar-link": "9616/486/637216416498708507.jpeg",
-      "name": "Lorenz",
-      "race": "Tiefling",
-      "classes": [{ "class": "Sorcerer", "level": 6 }],
+      name: "Lorenz",
+      race: "Tiefling",
+      classes: [{ class: "Sorcerer", level: 6 }],
       "session-count": 9,
-      "starting-level": 3
+      "starting-level": 3,
     },
     {
-      "id": 26114444,
+      id: 26114444,
       "player-dndbeyond-name": "Jonno0902",
       "avatar-link": "9765/125/1581111423-26114444.jpeg",
-      "name": "Luren",
-      "race": "Tiefling",
-      "classes": [{ "class": "Warlock", "level": 5 }],
+      name: "Luren",
+      race: "Tiefling",
+      classes: [{ class: "Warlock", level: 5 }],
       "session-count": 6,
-      "starting-level": 3
+      "starting-level": 3,
     },
     {
-      "id": 26109480,
+      id: 26109480,
       "player-dndbeyond-name": "ThePerkyRiolu",
       "avatar-link": "18/18/636378975214214687.jpeg",
-      "name": "Zaukan Thulaga Hornbearer",
-      "nickname": "Zak",
-      "race": "Goliath",
-      "classes": [{ "class": "Paladin", "level": 8 }],
+      name: "Zaukan Thulaga Hornbearer",
+      nickname: "Zak",
+      race: "Goliath",
+      classes: [{ class: "Paladin", level: 8 }],
       "session-count": 14,
-      "starting-level": 3
+      "starting-level": 3,
     },
     {
-      "id": 30318222,
+      id: 30318222,
       "player-dndbeyond-name": "SchemingPnda",
       "avatar-link": "11048/16/1581111423-30318222.jpeg",
-      "name": "Pipit",
-      "race": "Half-elf",
-      "subrace": "Wood",
-      "classes": [
-        { "class": "Ranger", "level": 4 },
-        { "class": "Rogue", "level": 2 }
+      name: "Pipit",
+      race: "Half-elf",
+      subrace: "Wood",
+      classes: [
+        { class: "Ranger", level: 4 },
+        { class: "Rogue", level: 2 },
       ],
       "session-count": 7,
-      "starting-level": 4
+      "starting-level": 4,
     },
     {
-      "id": 31690387,
+      id: 31690387,
       "player-dndbeyond-name": "Kosky101",
       "avatar-link": "11688/396/1581111423-31690387.jpeg",
-      "name": "Harold Stormberg",
-      "nickname": "Stormberg",
-      "race": "Human",
-      "classes": [
-        { "class": "Fighter", "level": 5 },
-        { "class": "Ranger", "level": 1 }
+      name: "Harold Stormberg",
+      nickname: "Stormberg",
+      race: "Human",
+      classes: [
+        { class: "Fighter", level: 5 },
+        { class: "Ranger", level: 1 },
       ],
       "session-count": 6,
-      "starting-level": 4
+      "starting-level": 4,
     },
     {
-      "id": 26133759,
+      id: 26133759,
       "player-dndbeyond-name": "Georgeousness",
       "avatar-link": "9765/428/1581111423-26133759.jpeg",
-      "name": "Qiharice",
-      "race": "Halfling",
-      "subrace": "Lightfoot",
-      "classes": [{ "class": "Bard", "level": 6 }],
+      name: "Qiharice",
+      race: "Halfling",
+      subrace: "Lightfoot",
+      classes: [{ class: "Bard", level: 6 }],
       "session-count": 7,
-      "starting-level": 3
+      "starting-level": 3,
     },
     {
-      "id": 26521341,
+      id: 26521341,
       "player-dndbeyond-name": "Crabbi",
       "avatar-link": "9765/133/1581111423-26521341.jpeg",
-      "name": "Thurdun Kragbrew",
-      "nickname": "Thurdun",
-      "race": "Dwarf",
-      "classes": [{ "class": "Bard", "level": 3 }],
+      name: "Thurdun Kragbrew",
+      nickname: "Thurdun",
+      race: "Dwarf",
+      classes: [{ class: "Bard", level: 3 }],
       "session-count": 1,
-      "starting-level": 3
+      "starting-level": 3,
     },
     {
-      "id": 34130081,
+      id: 34130081,
       "player-dndbeyond-name": "user-101304394",
       "avatar-link": "12250/463/1581111423-34130081.jpeg",
-      "name": "Utredyn Gurvusath",
-      "nickname": "Utredyn",
-      "race": "Triton",
-      "classes": [{ "class": "Barbarian", "level": 6 }],
+      name: "Utredyn Gurvusath",
+      nickname: "Utredyn",
+      race: "Triton",
+      classes: [{ class: "Barbarian", level: 6 }],
       "session-count": 2,
-      "starting-level": 5
-    }
+      "starting-level": 5,
+    },
   ],
-  "sessions": [
+  sessions: [
     {
-      "name": "WANTED: Iron Kara",
+      name: "WANTED: Iron Kara",
       "dungeon-master": "FreemDeem",
       "suggested-by": "Chaia",
       "suggested-date": "2020 09 03",
       "scheduled-date": "2020 10 01",
-      "players": ["Arthur", "Chaia", "Hans", "Pipit", "Utredyn"],
+      players: ["Arthur", "Chaia", "Hans", "Pipit", "Utredyn"],
       "max-players": 5,
-      "discord-channel": "#session-2"
+      "discord-channel": "#session-2",
     },
     {
-      "name": "Epic Historical Adventure",
+      name: "Epic Historical Adventure",
       "dungeon-master": "JackClarke",
       "suggested-by": "Jack",
       "suggested-date": "2020 09 05",
-      "players": ["Benji", "Owen", "Rowan", "Sarah"],
+      players: ["Benji", "Owen", "Rowan", "Sarah"],
       "max-players": 5,
-      "discord-channel": "#epic-adventure"
+      "discord-channel": "#epic-adventure",
     },
     {
-      "name": "Mumsy's Extra Planar Adventures",
+      name: "Mumsy's Extra Planar Adventures",
       "dungeon-master": "SunRester",
       "suggested-by": "Dave",
-      "players": ["Arthur", "Big Percy", "Esyln", "Qiharice"],
-      "max-players": 5
+      "suggested-date": "2020 09 01",
+      players: ["Arthur", "Big Percy", "Esyln", "Qiharice"],
+      "max-players": 5,
     },
     {
-      "name": "TEST - Space Available - Scheduled",
+      name: "TEST - Space Available - Scheduled",
       "dungeon-master": "SchemingPnda",
       "suggested-by": "Louis",
       "suggested-date": "2020 09 06",
       "scheduled-date": "2020 10 21",
-      "players": ["Arthur", "Big Percy", "Esyln", "Qiharice"],
-      "max-players": 5
+      players: ["Arthur", "Big Percy", "Esyln", "Qiharice"],
+      "max-players": 5,
     },
     {
-      "name": "TEST - Full Available - Not Scheduled",
+      name: "TEST - Full Available - Not Scheduled",
       "dungeon-master": "SunRester",
       "suggested-by": "Dave",
       "suggested-date": "2020 09 02",
-      "players": ["Arthur", "Big Percy", "Esyln", "Qiharice"],
-      "max-players": 4
-    }
+      players: ["Arthur", "Big Percy", "Esyln", "Qiharice"],
+      "max-players": 4,
+    },
   ],
   "link-groups": [
     {
-      "name": "D&D Beyond Campaign 1",
-      "links": [
+      name: "D&D Beyond Campaign 1",
+      links: [
         {
-          "name": "Campaign Link",
-          "url": "https://www.dndbeyond.com/campaigns/1003579"
+          name: "Campaign Link",
+          url: "https://www.dndbeyond.com/campaigns/1003579",
         },
         {
-          "name": "Invite Link",
-          "url": "https://ddb.ac/campaigns/join/10035793062304491"
-        }
-      ]
+          name: "Invite Link",
+          url: "https://ddb.ac/campaigns/join/10035793062304491",
+        },
+      ],
     },
     {
-      "name": "D&D Beyond Campaign 2",
-      "links": [
+      name: "D&D Beyond Campaign 2",
+      links: [
         {
-          "name": "Campaign Link",
-          "url": "https://www.dndbeyond.com/campaigns/1207080"
+          name: "Campaign Link",
+          url: "https://www.dndbeyond.com/campaigns/1207080",
         },
         {
-          "name": "Invite Link",
-          "url": "https://ddb.ac/campaigns/join/12070802660007381"
-        }
-      ]
+          name: "Invite Link",
+          url: "https://ddb.ac/campaigns/join/12070802660007381",
+        },
+      ],
     },
     {
-      "name": "LegendKeeper",
-      "links": [
+      name: "LegendKeeper",
+      links: [
         {
-          "name": "Map",
-          "url": "https://app.legendkeeper.com/a/worlds/cjz6tm8r3f2nq0796l89hyl6l/atlas/ck8ncyigln39v0790a31w1x6h"
+          name: "Map",
+          url:
+            "https://app.legendkeeper.com/a/worlds/cjz6tm8r3f2nq0796l89hyl6l/atlas/ck8ncyigln39v0790a31w1x6h",
         },
         {
-          "name": "Westeridge",
-          "url": "https://app.legendkeeper.com/a/worlds/cjz6tm8r3f2nq0796l89hyl6l/wiki/ck8nlecpwr3q60756mj8rm1qu"
+          name: "Westeridge",
+          url:
+            "https://app.legendkeeper.com/a/worlds/cjz6tm8r3f2nq0796l89hyl6l/wiki/ck8nlecpwr3q60756mj8rm1qu",
         },
         {
-          "name": "Campaign Log",
-          "url": "https://app.legendkeeper.com/a/worlds/cjz6tm8r3f2nq0796l89hyl6l/wiki/ck8uslol4dco60790e3bfmsz2"
-        }
-      ]
+          name: "Campaign Log",
+          url:
+            "https://app.legendkeeper.com/a/worlds/cjz6tm8r3f2nq0796l89hyl6l/wiki/ck8uslol4dco60790e3bfmsz2",
+        },
+      ],
     },
     {
-      "name": "Homebrew",
-      "links": [
+      name: "Homebrew",
+      links: [
         {
-          "name": "Strongholds and Followers",
-          "url": "https://app.legendkeeper.com/a/worlds/cjz6tm8r3f2nq0796l89hyl6l/atlas/ck8ncyigln39v0790a31w1x6h"
+          name: "Strongholds and Followers",
+          url:
+            "https://app.legendkeeper.com/a/worlds/cjz6tm8r3f2nq0796l89hyl6l/atlas/ck8ncyigln39v0790a31w1x6h",
         },
         {
-          "name": "Downtime Activities",
-          "url": "https://1drv.ms/b/s!AosWm-HIqV1WkKMQI61LjkNJ2Tc85w?e=CJSo12"
-        }
-      ]
-    }
+          name: "Downtime Activities",
+          url: "https://1drv.ms/b/s!AosWm-HIqV1WkKMQI61LjkNJ2Tc85w?e=CJSo12",
+        },
+      ],
+    },
   ],
-  "players": [
+  players: [
     {
-      "name": "Jack",
+      name: "Jack",
       "dndbeyond-name": "FreemDeem",
-      "screen-name": "JBC"
+      "screen-name": "JBC",
     },
     {
-      "name": "Lewis",
-      "dndbeyond-name": "Proxysaurus"
+      name: "Lewis",
+      "dndbeyond-name": "Proxysaurus",
     },
     {
-      "name": "Dave",
+      name: "Dave",
       "dndbeyond-name": "SunRester",
-      "screen-name": "Dave"
+      "screen-name": "Dave",
     },
     {
-      "name": "Rowan",
-      "dndbeyond-name": "AnAxeCalledYorik"
+      name: "Rowan",
+      "dndbeyond-name": "AnAxeCalledYorik",
     },
     {
-      "name": "Cassie",
-      "dndbeyond-name": "Chaia01"
+      name: "Cassie",
+      "dndbeyond-name": "Chaia01",
     },
     {
-      "name": "Jack",
+      name: "Jack",
       "dndbeyond-name": "JackClarke",
-      "screen-name": "Jack"
+      "screen-name": "Jack",
     },
     {
-      "name": "Sarah",
-      "dndbeyond-name": "BlackRoseSarah89"
+      name: "Sarah",
+      "dndbeyond-name": "BlackRoseSarah89",
     },
     {
-      "name": "Louis",
+      name: "Louis",
       "dndbeyond-name": "SchemingPnda",
-      "screen-name": "Louis"
+      "screen-name": "Louis",
     },
     {
-      "name": "Kosky",
-      "dndbeyond-name": "Kosky101"
+      name: "Kosky",
+      "dndbeyond-name": "Kosky101",
     },
     {
-      "name": "Benjy",
-      "dndbeyond-name": "Benjywa"
+      name: "Benjy",
+      "dndbeyond-name": "Benjywa",
     },
     {
-      "name": "Otis",
-      "dndbeyond-name": "TheKingsmantm"
+      name: "Otis",
+      "dndbeyond-name": "TheKingsmantm",
     },
     {
-      "name": "Jonno",
-      "dndbeyond-name": "Jonno0902"
+      name: "Jonno",
+      "dndbeyond-name": "Jonno0902",
     },
     {
-      "name": "George",
-      "dndbeyond-name": "Georgeousness"
+      name: "George",
+      "dndbeyond-name": "Georgeousness",
     },
     {
-      "name": "Andy",
-      "dndbeyond-name": "Brewhan"
+      name: "Andy",
+      "dndbeyond-name": "Brewhan",
     },
     {
-      "name": "Mike",
-      "dndbeyond-name": "Crabbi"
+      name: "Mike",
+      "dndbeyond-name": "Crabbi",
     },
     {
-      "name": "Pedro",
-      "dndbeyond-name": "user-101304394"
+      name: "Pedro",
+      "dndbeyond-name": "user-101304394",
     },
     {
-      "name": "Owen",
-      "dndbeyond-name": "ThePerkyRiolu"
-    }
-  ]
-}
+      name: "Owen",
+      "dndbeyond-name": "ThePerkyRiolu",
+    },
+  ],
+};
 
 export default Data;
