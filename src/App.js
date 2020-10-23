e3f255cc7976e7d57d16dd0e44bbe5b004c63b1e
Jack Clarke <jackclarke95@gmail.com>
2020-10-23 23:42:57 +0100

    style sign in/out

--- src/App.js
+++ src/App.js
@@ -37,9 +37,16 @@ class App extends React.Component {
         {this.state.currentUser ? (
           <>
             <h1>Into The West Campaign Tracker</h1>
-            <button className="sign-out-button" onClick={() => auth.signOut()}>
-              Sign Out
-            </button>
+            <div className="user-menu">
+              <div className="user-info">
+                Signed in as: {this.state.currentUser.displayName}
+                <div className="spacer">|</div>
+                <div className="sign-out-button" onClick={() => auth.signOut()}>
+                  Sign Out
+                </div>
+              </div>
+              <img src={this.state.currentUser.photoURL} />
+            </div>
             <NextAdventure sessions={Data.sessions} players={Data.players} />
             <div className="info">
               <Links linkGroups={Data["link-groups"]} />
@@ -59,13 +66,29 @@ class App extends React.Component {
               players={Data.players}
               characters={Data.characters}
             />
-
             {/* TODO: Graveyard */}
             {/* TODO: Player shop */}
             <DeploymentDate />
           </>
         ) : (
-          <button onClick={signInWithGoogle}>SIGN IN WITH GOOGLE</button>
+          <>
+            <h1>Into the West Campaign Tracker</h1>
+            <div className="sign-in-container">
+              <div className="sign-in-dialog">
+                <div>
+                  Welcome to the Campaign Tracker. Please sign in to continue:
+                </div>
+                <div className="button-container">
+                  <button onClick={signInWithGoogle}>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1004px-Google_%22G%22_Logo.svg.png" />
+                    <div className="google-sign-in-button">
+                      Sign In with Google
+                    </div>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </>
         )}
       </>
     );
